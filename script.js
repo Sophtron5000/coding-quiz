d9e901a7c11a4571e1f1487ebce911b1f849b9de
Sophia C <95388964+Sophtron5000@user.noreply.github.com>
2022-04-04 20:34:59 -0700

    add logic notes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //GIVEN I am taking a code quiz
 
-// WHEN I click the start button, 
+// WHEN I click the start button, (eventListener)
     //120 sec. timer starts setTimer() (in code drills)
-        //questions become visible
-            //if question is answered correctly
+        //questions become visible(hidden numbered card activity)
+            //if question is answered correctly (for loop + if else)
                 //present next question
                 //else subtract time from the clock
-   //when timer runs out game over || all answers true
-    //else log in local storage score(time left) + initials
+   //when timer runs out game over || all answers true (end of function or timer?)
+    //else log in local storage score(time left) + initials (formSubmission activity)
