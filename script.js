95a44fc52acbfcccc567ac88186b117837728dc5
Sophia C <95388964+Sophtron5000@user.noreply.github.com>
2022-04-04 20:26:23 -0700

    psuedocode quiz logic

---
+++ script.js
@@ -0,0 +1,10 @@
+//GIVEN I am taking a code quiz
+
+// WHEN I click the start button, 
+    //120 sec. timer starts setTimer() (in code drills)
+        //questions become visible
+            //if question is answered correctly
+                //present next question
+                //else subtract time from the clock
+   //when timer runs out game over || all answers true
+    //else log in local storage score(time left) + initials
