d8178145392a854056f422e1d13445e2a6f70321
Sophia C <95388964+Sophtron5000@user.noreply.github.com>
2022-04-05 16:49:25 -0700

    add timer and firrst question functions

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 //GIVEN I am taking a code quiz
 
 // WHEN I click the start button, (eventListener)
+var start = document.querySelector('#quiz')
+var mode = 'end quiz'
     //120 sec. timer starts setTimer() (in code drills)
+    quiz.addEventListener('click', function() {
+        // setTimeout(timer, 1000 * 120);
+        // document.getElementById('#timer')
+        
+        // function timer(timeStart){
+        //     var timeStart = 12000
+        //     for (var i = 120000, i =< timeStart.length; i--)
+        // } timer[i]
         //questions become visible(hidden numbered card activity)
+        function question1(str) {
+        document.getElementById("#question").innerHTML = "What does 'getElementByID' do?"
+        document.getElementById('#answer1')
+        } div.append(div)
             //if question is answered correctly (for loop + if else)
                 //present next question
-                //else subtract time from the clock
+                //else subtract time from the clock 
+        setTimeout(quiz, 1000 * 120);
+    })
+   
+       
    //when timer runs out game over || all answers true (end of function or timer?)
     //else log in local storage score(time left) + initials (formSubmission activity)
